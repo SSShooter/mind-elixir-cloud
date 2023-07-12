9694900eb66bf4492d4e32c9aecbf213105acfb1
ssshooter <ssshooterx@gmail.com>
2023-07-12 23:06:58 +0800

    chore: add tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+  <NavBar />
+  <div></div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
