51b6e115de20cf63078c52d8403943a0abf8a894
ssshooter <ssshooterx@gmail.com>
2025-04-09 21:05:28 +0800

    chore: refactor DesktopLogin.vue and update tsconfig.json

--- src/views/DesktopLogin.vue
+++ src/views/DesktopLogin.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>
-    dddd
-  </div>
+  <div>dddd</div>
 </template>
 
 <script setup lang="ts">
+import { onMounted } from "vue";
+import connect from "@/connect";
+import { useRoute } from "vue-router";
 
+const route = useRoute();
+onMounted(async () => {
+  const { token } = await connect.get<never, { token: string }>("token");
+  console.log(token);
+  const port = route.query.port;
+  console.log(port);
+  const response = await fetch(
+    `http://127.0.0.1:${port}/login?token=${token}`,
+    {
+      method: "POST",
+    }
+  );
+  const data = await response.text();
+  console.log(data);
+});
 </script>
